1e13b0eb3efe231ad04262e0ef913d3e31b89084
Mahmudjon <mahmudjonrahimjonov007@gmail.com>
2025-05-12 14:29:29 +0500

    VUEcinema-React

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Logo from './assets/images/logo.svg'
+import menu from './assets/images/menu.png'
+import close from './assets/images/close.png'
 const Nav = () => {
   return (
       <>
@@ -10,7 +12,14 @@ const Nav = () => {
                 <img src={Logo} alt="" />
                 <span>CINEMAS</span>
               </a>
+              <label for="check" className='menu'>
+                <img src={menu} alt="" />
+              </label>
+              <input type="checkbox" id='check' />
               <ul className="nav_list">
+              <label for="check" className='close'>
+                <img src={close} alt="" />
+              </label>
                 <li><a href="#!" className="nav_link"><span>Films</span></a></li>
                 <li><a href="#!" className="nav_link"><span>Serials</span></a></li>
                 <li><a href="#!" className="nav_link"><span>Cartoons</span></a></li>
